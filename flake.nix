733ac486fb782bd34dc6576ffdcdd2a38e7752d8
Haridula <haridula@proton.me>
2026-07-29 16:04:01 +0300

    nixos config

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "NixOS configuration with Noctalia";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        inputs.noctalia.nixosModules.default
+        ./noctalia.nix
+      ];
+    };
+  };
+}
---
+++ noctalia.nix
@@ -0,0 +1,6 @@
+{ pkgs, inputs, ... }: {
+  programs.noctalia = {
+    enable = true;
+    systemd.enable = true;
+  };
+}
