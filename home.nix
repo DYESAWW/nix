b342ade440add2ad4fa3760b92d293b2802781df
Haridula <haridula@proton.me>
2026-07-30 20:39:58 +0300

    generic commit

--- home.nix
+++ home.nix
@@ -74,34 +74,34 @@ in
         flake = "sudo nano /etc/nixos/flake.nix";
         frebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
         commit = "cd /etc/nixos && sudo git add -A && sudo git commit -m 'generic commit'";
+      };
 
-        oh-my-zsh = {
-          enable = true;
-          plugins = ["git"];
-        };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" ];
+      };
 
-        plugins = [
-          {
-            name = "zsh-autocomplete";
-            src = pkgs.fetchFromGitHub {
-            owner = "marlonrichert";
-            repo = "zsh-autocomplete";
-            rev = "23.07.13";
-            sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
-            };
-          }
+      plugins = [
+        {
+          name = "zsh-autocomplete";
+          src = pkgs.fetchFromGitHub {
+          owner = "marlonrichert";
+          repo = "zsh-autocomplete";
+          rev = "23.07.13";
+          sha256 = "sha256-/6V6IHwB5p0GT1u5SAiUa20LjFDSrMo731jFBq/bnpw=";
+          };
+        }
 
-          {
-            name = "zsh-syntax-highlighting";
-            src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-syntax-highlighting";
-            rev = "0.8.0";
-            sha256 = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
-            };
-          }
-        ];
-      }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.8.0";
+          sha256 = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
+          };
+        }
+      ];
     };
 
     atuin = {
